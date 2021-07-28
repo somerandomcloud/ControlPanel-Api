132ab57032b475304739ea6cf6cd478d807696e2
SomeRandomCloud <samuel.loddo@gmail.com>
2021-07-28 16:42:20 +0200

    I messed it up

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ function delLast(str) {
 
 const checkUrl = (url) => {
 	return new Promise(async (resolve) => {
-		const res = string.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
+		const res = url.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
 		resolve(res !== null);
 	});
 };
